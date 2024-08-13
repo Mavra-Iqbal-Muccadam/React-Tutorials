4cfbc5d78bd3c281820cab83d36af234076b2b93
Mavra Iqbal Muccadam <mavra.iqbal@muccadam.com>
2024-08-13 18:48:40 +0500

    added new files to repo

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let sumData=(a,b)=>{
+    return a+b;
+}
+
+let subData=(a,b)=>{
+    return a-b;
+}
+
+let name="Mavra is learning REACT";
+// default export
+// export default sumData
+
+// named export
+export {sumData,subData,name}
---
+++ index.js
@@ -0,0 +1,11 @@
+// default import
+
+// import sumData from "./calculator.js";
+// console.log(sumData(10,20));
+
+// named import
+import {sumData,subData, name} from "./calculator.js";
+
+console.log(sumData(10,20));
+console.log(subData(10,20));
+console.log(name)
